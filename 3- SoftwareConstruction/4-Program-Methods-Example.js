7416805af4220d6bd09bf62d7f00c94c33eaffa3
ThaumBear <79795343+ThaumBear@users.noreply.github.com>
2022-11-19 03:14:57 +0200

    Update 4-Program-Methods-Example.js

--- 3- SoftwareConstruction/4-Program-Methods-Example.js
+++ 3- SoftwareConstruction/4-Program-Methods-Example.js
@@ -1,7 +1,7 @@
 import { mongoClient, user } from './user.js'
 
 //вивід списків компощицій, скомпонованих за жанром
-async function searchCompose(request_ID, request, res) {
+async function searchCompose(request_ID, request) {
     if (request_ID != 0 || request_ID != 1) {
         return -1;
     }
@@ -14,17 +14,16 @@ async function searchCompose(request_ID, request, res) {
     const composeList = await collection.find().toArray();
     await client.close();
     console.log(composeList);
-    res.send(composeList)
-    return 1;
+    return composeList;
 }
 
 //вивід обраної користувачем композиції
-async function selectCompose(composeList, request, res) {
+async function selectCompose(composeList, request) {
     await client.connect();
     const db = client.db('Project');
     const collection = await db.collection("Обрана композиція");
     Composition composition = await collection.find();
     await client.close();
     console.log(composition);
-    res.send(composition)
+    return composition
 }
