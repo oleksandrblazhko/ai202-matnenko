6afa1859efc7d9c00e981218ad5ff183272970e5
ThaumBear <79795343+ThaumBear@users.noreply.github.com>
2022-11-21 01:30:28 +0200

    Update 5-Program-Methods-Using-Example.js

--- 3- SoftwareConstruction/5-Program-Methods-Using-Example.js
+++ 3- SoftwareConstruction/5-Program-Methods-Using-Example.js
@@ -1,9 +1,9 @@
-let requestID = 1;
-let author = "Вербицький";
-let composeName = "Гімн України";
+const requestID = 1;
+const author = 'Вербицький';
+const composeName = 'Гімн України';
 
 //вивід списків композицій заданого автора, скомпонованих за жанром
-ComposeList musicList = searchCompose(requestID, author)
+console.log(searchCompose(requestID, author))
 
 //вивід обраної користувачем композиції заданого автора
-Composition composition = selectCompose(musicList, composeName)
+console.log(selectCompose(musicList, composeName))
