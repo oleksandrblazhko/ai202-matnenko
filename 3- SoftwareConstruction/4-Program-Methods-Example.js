1702af4c2443dc20afa872d6349d59495a7cfcb1
ThaumBear <79795343+ThaumBear@users.noreply.github.com>
2022-11-21 01:24:25 +0200

    Update 4-Program-Methods-Example.js

--- 3- SoftwareConstruction/4-Program-Methods-Example.js
+++ 3- SoftwareConstruction/4-Program-Methods-Example.js
@@ -1,27 +1,28 @@
-import { mongoClient, user } from './user.js'
+import { client, db } from './user.js'
 
-//вивід списків компощицій, скомпонованих за жанром
-async function searchCompose(request_ID, request) {
-    if (request_ID != 0 || request_ID != 1) {
-        return -1;
-    }
-    if (request.length < 1 || request.length > 64) {
-        return -2
-    }
-    await client.connect();
-    const db = client.db('Project');
-    const collection = await db.collection("Списки композицій, скомпоновані за жанром");
-    const composeList = await collection.find().toArray();
-    await client.close();
-    return composeList;
+//вивід списків композицій, скомпонованих за жанром
+
+async function searchCompose (requestID, request) {
+  if (requestID !== 0 || requestID !== 1) {
+    return -1
+  }
+  if (request.length < 1 || request.length > 64) {
+    return -2
+  }
+  await client.connect()
+  const db = client.db('Project')
+  const collection = await db.collection('Списки композицій, скомпоновані за жанром')
+  const composeList = await collection.find().toArray()
+  await client.close()
+  return composeList
 }
 
 //вивід обраної користувачем композиції
-async function selectCompose(composeList, request) {
-    await client.connect();
-    const db = client.db('Project');
-    const collection = await db.collection("Обрана композиція");
-    Composition composition = await collection.find();
-    await client.close();
-    return composition;
+async function selectCompose (composeList, request) {
+   await client.connect()
+   const db = client.db('Project')
+   const collection = await db.collection("Обрана композиція")
+   Composition composition = await collection.find()
+   await client.close()
+   return composition
 }
